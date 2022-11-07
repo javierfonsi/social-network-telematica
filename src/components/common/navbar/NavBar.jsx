7d83b4368a2a22ccce34d5545f3d24185b0bce8a
javierfonsi <javier_fonsi@hotmail.com>
2022-11-06 23:23:50 -0500

    Home

--- src/components/common/navbar/NavBar.jsx
+++ src/components/common/navbar/NavBar.jsx
@@ -9,14 +9,14 @@ const NavBar = () => {
   return (
     <div className='container-navbar'>
       <div className="opt-req">
-        <Button variant="contained" size="small" color="secondary" type='on'>Mensajes</Button>
-        <Button variant="contained" size="small" color="info" type='submit'>Promos</Button>
-        <Button variant="contained" size="small" color="info" type='submit'>Ventas</Button>
+        <Button className='req-but-header' variant="contained" color="secondary" type='on'>Mensaje</Button>
+        <Button className='for-but-header' variant="contained" color="info" type='submit'>Promos</Button>
+        <Button className='for-but-header' variant="contained" color="info" type='submit'>Ventas</Button>
       </div>
         <img className='logo' src={imagen} alt="logo-telematica" />
       <div className="opt-owner">
-        <Button variant="contained" size="small" color="success" type='submit'>Perfil</Button>
-        <Button variant="contained" size="small" color="error" onClick={() => {localStorage.clear()
+        <Button className='for-but-header' variant="contained" color="success" type='submit'>Perfil</Button>
+        <Button className='for-but-header' variant="contained" color="error" onClick={() => {localStorage.clear()
         navigate('/')}}>Salir</Button>
       </div>
     </div>
