993a0805b51d380e384fa89a465284d4f18d882f
javierfonsi <javier_fonsi@hotmail.com>
2022-11-13 05:32:19 -0500

    loading

---
+++ src/components/common/loading/Loading.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Stack from '@mui/material/Stack';
+import CircularProgress from '@mui/material/CircularProgress';
+
+
+const Loading = () => {
+  return (
+    <div>    
+        <Stack sx={{ color: 'grey.500' }} spacing={2} direction="row">
+            <CircularProgress color="secondary" />
+            <CircularProgress color="success" />
+            <CircularProgress color="inherit" />
+        </Stack>
+    </div>
+  )
+}
+
+export default Loading
--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,23 +1,24 @@
-import React, { useEffect } from 'react'
-
+import React, { useEffect, useState } from 'react'
 import './Home.css'
 import {useDispatch, useSelector} from 'react-redux'
 import Card from '../../common/Card'
 import { getUsers } from '../../../store/users/thunk'
 import Footer from '../../layaout/footerLayout/Footer'
+import Loading from '../../common/loading/Loading'
 
 const Home = () => {
   const dispatch = useDispatch()
 
   const {users} = useSelector((state)=>state.users)
+  const [load, setLoad] = useState(false)
 
   //console.log(users)
 
     useEffect(() =>{
       dispatch(getUsers())
-     }, [dispatch])
+      users? setLoad(false): setLoad(true)
+     }, [dispatch, users])
 
-     console.log(users)
 
 
 
@@ -25,14 +26,17 @@ const Home = () => {
   return (
     <div className='home-main'>
       <h2 className="current-user">Usuarios actuales</h2>
-      <div className='card-main'>
         {
-          users.map(user=>(
-            <Card user = {user} key={user.id}/>
-          ))
+          load? <div className="container-load"><Loading/></div> :         
+          <div className='card-main'>
+            {
+              users.map(user=>(
+                <Card user = {user} key={user.id}/>
+              ))
+            }
+          </div>
         }
-      </div>
-      <Footer/>
+        <Footer/>
     </div>
   )
 }
